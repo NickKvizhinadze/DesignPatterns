ad66bcccd3450a11268465b17da29517ea7a42a1
= <=>
2019-07-31 09:39:04 +0400

    Added Flyweight pattern text formatting project

---
+++ DesignPatterns/Flyweight/Flyweight.TextFormatting/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flyweight.TextFormatting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ft = new FormattedText("This is a barve new wolrd");
+            ft.Capitalize(10, 15);
+            Console.WriteLine(ft.ToString());
+
+            var bft = new BetterFormattedText("This is a barve new wolrd");
+            bft.GetRange(10, 15).Capitalize = true;
+            Console.WriteLine(bft.ToString());
+
+        }
+    }
+}
